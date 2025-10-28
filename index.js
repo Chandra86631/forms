c84887dd53fa59c2357dbf2e3bdf0104e936f386
jaya <jayachandrav2002@gmail.com>
2025-10-28 14:35:18 +0530

    Delete index.js
    g

--- index.js
+++
@@ -1,9 +0,0 @@
-function scrollToContact() {
-  document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
-}
-
-document.getElementById('contactForm').addEventListener('submit', function (e) {
-  e.preventDefault();
-  document.getElementById('formMessage').textContent = 'Thanks for reaching out! We’ll get back to you soon.';
-  this.reset();
-});
